a2f50d90c785e143a944da2fe24b9d0d5cd91970
chianye <auroraleecy@gmail.com>
2025-11-20 17:10:28 +0900

    resolving merging conflicts

---
+++ frontend/components/icons/nuggit-icon.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { ProfileIcon } from '@/components/icons/profile-icon';
+
+interface NuggitIconProps {
+  size?: number;
+}
+
+export function NuggitIcon({ size = 50 }: NuggitIconProps) {
+  return (
+    <View style={{ width: size, height: size }}>
+      {/* Default profile icon */}
+      <ProfileIcon size={size} />
+
+      {/* Nuggit image overlay (centered, default size) */}
+      <Image
+        source={require('@/assets/images/nuggit-icon.png')}
+        style={styles.overlay}
+        resizeMode="contain"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [
+      { translateX: -0.5 * 37 },  
+      { translateY: -0.5 * 50 },  
+    ],
+    width: 37,   
+    height: 50,  
+  },
+});
+
