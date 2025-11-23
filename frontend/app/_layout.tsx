3bcc3bc35e0f62d4c411026c03277d698ed83099
nadiarvi <nadia.arvi@gmail.com>
2025-11-23 18:04:10 +0900

    connect nudge and chat button

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout() {
               title: '',
               headerBackTitle: '',
               headerRight: () => <TaskDetailHeader />,
+              gestureEnabled: false,
             }} 
           />
         </Stack>
